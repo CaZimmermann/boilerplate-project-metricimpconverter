76fb15a4692d96b01aabaa376bc9cd5f2721a48a
CaZimmermann <103119813+CaZimmermann@users.noreply.github.com>
2025-02-11 18:25:13 +0100

    fix: added two missing tests

--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -17,6 +17,12 @@ suite('Unit Tests', function(){
     test('convertHandler should correctly read a fractional input with a decimal', function () {
         assert.equal(convertHandler.getNum('2.5/5L'),  0.5);
     });
+    test('convertHandler should correctly return an error on a double-fraction (i.e. 3/2/3)', function () {
+        assert.equal(convertHandler.getNum('3/2/3'),  null);
+    });
+    test('convertHandler should correctly default to a numerical input of 1 when no numerical input is provided', function () {
+        assert.equal(convertHandler.getNum('kg'),  1);
+    });
     test('convertHandler should correctly read each valid input unit', function () {
         let units = ['gal', 'L', 'mi', 'km', 'lbs', 'kg'];
         units.forEach(unit => {
